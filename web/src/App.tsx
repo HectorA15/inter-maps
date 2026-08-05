5818515555f5cce8571701a3e69ceee39e66ff54
Hector Armando <crusch1503@gmail.com>
2026-08-04 22:30:23 -0600

    feat: inicializar cliente web con Vite e integrar README

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
+        <h1>InterMaps</h1>
+        <p>Interfaz local del robot operativa.</p>
+      </div>
+  );
+}
+
+export default App;
