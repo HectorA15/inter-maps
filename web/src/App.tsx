6ff3826b8d2ddbc266eebb21d069c1fc09b3b6cb
Hector Armando <crusch1503@gmail.com>
2026-08-17 00:24:34 -0600

    Se modifico el buscador para contener un droptown y mostrar las coincidencias con busquedas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -28,19 +28,7 @@ function App() {
 
   return (
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
-        <h1>InterMaps</h1>
           <Buscador/>
-        <p>Interfaz local del robot operativa.</p>
-          {
-              edificios.map((edificio) => (
-                  <div key={edificio.id}>
-                      <h2>Edificio {edificio.nombre}</h2>
-                      <p>Espacios: {edificio.espacios.length}</p>
-                  </div>
-
-
-              ))
-          }
       </div>
   );
 }
--- web/src/components/Buscador.tsx
+++ web/src/components/Buscador.tsx
@@ -1,27 +1,54 @@
+import { useState, useEffect } from 'react';
+import { CatalogoService } from '../services/apiClient';
 
-import { useState } from 'react';
-import type { Edificio } from '../interfaces/ApiInterfaces';
-
-interface BuscadorProps {
-    titulo?: string;
-}
-
-export function Buscador({}: BuscadorProps) {
-
+export function Buscador() {
     const [textoBusqueda, setTextoBusqueda] = useState<string>("");
+    const [resultados, setResultados] = useState<SearchResult[]>([]);
+
+    useEffect(() => {
+        if (textoBusqueda.trim() === '') {
+            setResultados([]);
+            return;
+        }
+
+        const temporizador = setTimeout(async () => {
+            try {
+                const resultadosBusqueda = await CatalogoService.buscar(textoBusqueda);
+                setResultados(resultadosBusqueda);
+                console.log("Buscando:", textoBusqueda);
+            } catch (error) {
+                console.error("Error en la busqueda:", error);
+            }
+        }, 500);
+
+        return () => clearTimeout(temporizador);``
+
+    }, [textoBusqueda]);
 
     const manejarCambio = (nuevoTexto: string) => {
         setTextoBusqueda(nuevoTexto);
     };
 
-
     return (
-        <>
+        <div className="contenedor-buscador-relativo">
             <input
+                className="input-buscador"
                 type="text"
+                placeholder="Buscar lugar o edificio..."
                 value={textoBusqueda}
                 onChange={(e) => manejarCambio(e.target.value)}
             />
-        </>
+
+            {/* Solo dibuja el menú si la memoria tiene datos */}
+            {resultados.length > 0 && (
+                <ul className="dropdown-resultados">
+                    {resultados.map((item) => (
+                        <li key={`${item.tipo}-${item.id}`} className="item-resultado">
+                            {item.nombre}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     );
 }
