67e824cd8d30eb7248eaf5f0fcd4c780b98dda19
HectorA15 <crusch1503@gmail.com>
2026-09-20 15:55:48 -0600

    cambios en vite.config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -81,7 +81,7 @@ export default defineConfig({
   },
 
   worker: {
-    format: "es",
+    format: "iife",
     // Por si Vite empaqueta el worker por separado, forzamos también aquí el .js
     rollupOptions: {
       output: {
